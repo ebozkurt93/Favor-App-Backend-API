30ec0a178526d0af7f1f6b5fa37e028f81c6bada
Erdem Bozkurt <ebozkurt93@gmail.com>
2017-09-03 18:59:06 +0300

    String trim for user class BS-16

--- src/main/java/com/favorapp/api/user/User.java
+++ src/main/java/com/favorapp/api/user/User.java
@@ -27,116 +27,111 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 @Entity
 public class User {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int id;
-	@Column(nullable = false)
-	private String name;
-	@Column(nullable = false)
-	private String lastname;
-	@Column(nullable = false)
-	private String email;
-	@Column(nullable = false)
-	@Temporal(TemporalType.DATE)
-	@JsonFormat(pattern="yyyy-MM-dd")
-	private Date birthDate;
-	@Temporal(TemporalType.DATE)
-	@JsonFormat(pattern="yyyy-MM-dd")
-	private Date registerDate;
-	@Column(nullable = false)
-	private String password;
-	
-	@ElementCollection(targetClass = Role.class)
-	@JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
-	@Column(name = "roles", nullable = false)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String lastname;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    @Temporal(TemporalType.DATE)
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date birthDate;
+    @Temporal(TemporalType.DATE)
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date registerDate;
+    @Column(nullable = false)
+    private String password;
+
+    @ElementCollection(targetClass = Role.class)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "roles", nullable = false)
     @Enumerated(EnumType.STRING)
     private Collection<Role> roles;
-	
-	//private String phoneNumber;
-	// profile pic
-	// private BufferedImage profilePic;
-	// private Image profilePic;
-	//password etc
-	/*
+
+    //private String phoneNumber;
+    // profile pic
+    // private BufferedImage profilePic;
+    // private Image profilePic;
+    //password etc
+    /*
 	 * private String address; private String
 	 * email;
 	 */
 
-	public User() {
-		super();
-	}
+    public User() {
+        super();
+    }
 
-	public int getId() {
-		return id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
 
-	public String getLastname() {
-		return lastname;
-	}
+    public String getLastname() {
+        return lastname;
+    }
 
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
+    public void setLastname(String lastname) {
+        this.lastname = lastname == null ? null : lastname.trim();
+    }
 
-	public Date getBirthDate() {
-		return birthDate;
-	}
+    public Date getBirthDate() {
+        return birthDate;
+    }
 
-	public void setBirthDate(Date birthDate) {
-		this.birthDate = birthDate;	
-	}
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
 
-	public Date getRegisterDate() {
-		return registerDate;
-	}
+    public Date getRegisterDate() {
+        return registerDate;
+    }
 
-	public void setRegisterDate(Date registerDate) {
-		this.registerDate = registerDate;
-	}
+    public void setRegisterDate(Date registerDate) {
+        this.registerDate = registerDate;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	public Collection<Role> getRoles() {
-		if (roles == null){
-			roles = new ArrayList<Role>();
-		}
-		return roles;
-	}
-	
-	public void setRoles(Collection<Role> roles) {
-		this.roles = roles;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	
+    public Collection<Role> getRoles() {
+        if (roles == null) {
+            roles = new ArrayList<Role>();
+        }
+        return roles;
+    }
 
+    public void setRoles(Collection<Role> roles) {
+        this.roles = roles;
+    }
 
-	
-	
 
 }
